12b6bcb65ace339e097152b79e57714ecb770b71
lucasbruno1992 <llucasbruno92@gmail.com>
2022-06-26 11:25:26 -0300

    corrijo app2.js y creo app1.js

--- app2.js
+++ app2.js
@@ -1,12 +1,10 @@
-let palabra = prompt(`Ingrese una palabra`) //se le asigna a palabra lo que el cliente desee
+let entrada = prompt(`Ingrese texto o Esc para interrumpir`); //se le asigna a palabra lo que el cliente desee
+let texto = " "; //declaramos una variable texto para guargar la palabra
 
-while (palabra != "ESC" && palabra != "esc") { //entra en un while mientras que palabra sea distinto de ESC o de esc se ejecuta la condicion
-    alert(palabra) // se informa mediante un alert la palabra ingresada
-    palabra = prompt(`Ingrese otra palabra`) //se pide otra palabra y se ejecuta el while
+while (entrada != "esc" && entrada != "ESC"){ //mientras entrada sea distinto de esc se mete dentro de la condicion
+texto += entrada + " "; //que texto va a concatenar la palabra ingresada + un espacio
+entrada = prompt(`Ingrese texto o Esc para interrumpir`); // pide que ingrese otra palabra para ver si se mete o no en el while
 }
 
-while (palabra != "ESC" && palabra != "esc") { //entra en un while mientras que palabra sea distinto de ESC o de esc se ejecuta la condicion
-    palabra = prompt(`Ingrese otra palabra`) //se pide otra palabra y se ejecuta el while
-    frase = palabra + " " + palabra
-    alert(frase) // se informa mediante un alert la palabra ingresada
-}
+alert(texto); //muestra por un alert la frase que se creo
+
